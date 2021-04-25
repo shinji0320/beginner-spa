684e071714b04d3c2486556ad38a12165f6d5a5c
shinji0320 <shinji.blue.one@gmail.com>
2021-04-25 18:11:05 +0900

    [add]ルーティング追加

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Button from "./components/Button";
 import Drinks from "./containers/Drinks";
 import First from "./containers/First";
@@ -9,16 +12,44 @@ import Show from "./containers/Show";
 import Third from "./containers/Third";
 
 function App() {
-  return <>
-    <Button />
-    <Drinks />
-    <First />
-    <Foods />
-    <Index />
-    <Second />
-    <Show />
-    <Third />
-  </>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/buttons">
+            <Button />
+            <Link to="/">Home</Link>
+          </Route>
+
+          <Route exact path="/menus">
+            <Link to="/">Home</Link>
+            <Drinks />
+            <Foods />
+          </Route>
+
+          <Route exact path="/first">
+            <Link to="/">Home</Link>
+            <First />
+          </Route>
+
+          <Route exact path="/items">
+            <Link to="/">Home</Link>
+            <Index />
+            <Show />
+          </Route>
+
+          <Route exact path="/">
+            <Link to="/buttons">Buttons</Link>
+            <Link to="/menus">Menus</Link>
+            <Link to="/first">First</Link>
+            <Link to="/items">items</Link>
+            <Second />
+            <Third />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 
 const Button = () => {
   return (
     <div>
+      <Link to="/">Home</Link>
       <button>ボタン</button>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
